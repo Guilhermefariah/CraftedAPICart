ebf58655812c9ecbed8e253be665f3a2ef5533fb
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-06-09 14:55:36 -0300

    Estrutura da pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,11 @@ interface PageProps {
 
 const Page: React.FC<PageProps> = ({ products }) => {
     return (
-        
+        <div className="container mx-auto py-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                test
+            </div>
+        </div>
     )
 }
+export default Page;
