4f007af1ac8dd8ee0e5897666efc6fd5c37cbd48
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-06-09 14:35:50 -0300

    Array de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,10 @@ import { GetStaticProps } from "next";
 import itemCard from "@/components/itemCard";
 
 interface PageProps {
-
+    products: {
+        id: string,
+        name: string,
+        price: number,
+        imageUrl: string
+    }[];
 }
